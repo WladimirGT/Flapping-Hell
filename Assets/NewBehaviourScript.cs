43c2efef106c35ea6251670b81ccc6a5f14f9886
WladimirGT <32730779+WladimirGT@users.noreply.github.com>
2017-10-12 05:48:47 -0300

    Fix NewBehaviourScript.cs mensaje

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -6,7 +6,7 @@ public class NewBehaviourScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Debug.Log ("Hola");
+		Debug.Log ("Hola esto esta corregido");
 	}
 	
 	// Update is called once per frame
